dc5f966d685935d79625e030fb6ca71bda8804e9
AlexM1tra <alex.mitra@ada.ac.uk>
2022-05-17 14:14:28 +0100

    Adds two test commands

--- src/main.py
+++ src/main.py
@@ -1,8 +1,15 @@
+import os
 import sys
 
 def main():
     command = sys.argv[1]
-    print(command)
+    if command == "commit":
+        name = sys.argv[2]
+        os.system("git commit -m '" + name + "'")
+    elif command == "uc":
+        os.system("git push")
+    else:
+        print("Unknown gum command")
 
 if __name__ == '__main__':
     main()
