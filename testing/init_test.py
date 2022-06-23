ae11dfa0730b7b608cfcce1b155cf3fe55b304cc
Mitra0000 <mitra11072@gmail.com>
2022-06-23 18:23:30 +0100

    Writes first integration test.

--- testing/init_test.py
+++ testing/init_test.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from integration import IntegrationTest
@@ -5,7 +6,7 @@ from integration import IntegrationTest
 # Integration tests for the `gm init` command.
 class InitTest(IntegrationTest):
     def testInitCommandOnEmptyRepository(self):
-        self.runCommand(f"{self.GUM} init")
+        self.createFirstCommit()
         branches = self.runCommand("git branch")
         self.assertEqual(branches, "* head\n")
 
--- testing/integration.py
+++ testing/integration.py
@@ -1,20 +1,23 @@
 import os
 import shutil
 import subprocess
+import tempfile
 import unittest
 
 import context
 
 # Template for integration tests to inherit from.
 class IntegrationTest(unittest.TestCase):
-    TEST_REPOSITORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), "repository")
+    TEST_REPOSITORY = os.path.join(tempfile.gettempdir(), "gumtesting", "sandbox", "repository")
+    REMOVE_WHEN_DONE = os.path.join(tempfile.gettempdir(), "gumtesting")
     GUM = "python3 " + os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "src", "main.py ")
+
     def setUp(self):
-        os.mkdir(self.TEST_REPOSITORY)
-        self.runCommand("git submodule init")
+        os.makedirs(self.TEST_REPOSITORY)
+        self.runCommand("git init")
         
     def tearDown(self):
-        shutil.rmtree(self.TEST_REPOSITORY)
+        shutil.rmtree(self.REMOVE_WHEN_DONE)
     
     def testRepository(self):
         self.assertTrue(os.path.exists(self.TEST_REPOSITORY))
@@ -24,6 +27,13 @@ class IntegrationTest(unittest.TestCase):
         out, err = process.communicate()
         process.wait()
         return out.decode("utf-8")
+    
+    def createFirstCommit(self):
+        self.runCommand(f"{self.GUM} init")
+        with open(os.path.join(self.TEST_REPOSITORY, "test.txt"), "w") as f:
+            f.write("This is a test.")
+        self.runCommand("git add -A")
+        self.runCommand("git commit -m 'Initial_commit.'")
 
 if __name__ == '__main__':
     unittest.main()
