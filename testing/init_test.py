7eef4de9bb60086ea45fa30b81b5f8f53753272d
Mitra0000 <mitra11072@gmail.com>
2022-06-23 18:30:39 +0100

    Adds another integratino test for init command.

--- testing/init_test.py
+++ testing/init_test.py
@@ -9,6 +9,18 @@ class InitTest(IntegrationTest):
         self.createFirstCommit()
         branches = self.runCommand("git branch")
         self.assertEqual(branches, "* head\n")
+    
+    def testInitCommandOnRepositoryWithBranches(self):
+        self.createFirstCommit()
+        self.runCommand("git checkout -b testBranch")
+        self.runCommand("git checkout -b otherBranch")
+        with open(os.path.join(self.TEST_REPOSITORY, "newfile.txt"), "w") as f:
+            f.write("This is a new file.")
+        self.runCommand("git add -A")
+        self.runCommand("git commit -m 'Second_commit'")
+        self.runCommand(f"{self.GUM} init")
+        branches = self.runCommand("git branch")
+        self.assertEqual(branches, "* head\n")
 
 if __name__ == '__main__':
     unittest.main()
