4d49bb9e447d11bfe4e5cb56ba1c08bbbeb6ade9
Mitra0000 <mitra11072@gmail.com>
2022-07-05 12:56:57 +0100

    Adds integration tests for diff command.

--- testing/diff_test.py
+++ testing/diff_test.py
@@ -5,7 +5,34 @@ from integration import IntegrationTest
 
 # Integration tests for the `gm diff` command.
 class DiffTest(IntegrationTest):
-    pass
+    def testEmptyDiff(self):
+        self.assertEqual(self.runCommand(f"{self.GUM} diff"), "")
+        self.assertGumOutputEqualsGitOutput()
+
+    def testAddedDiff(self):
+        self.createFile("newfile.txt", "This is a new file.")
+        self.assertGumOutputEqualsGitOutput()
+
+    def testModifiedDiff(self):
+        self.createFirstCommit()
+        self.modifyFile("test.txt", "This is a modified test.")
+        self.assertGumOutputEqualsGitOutput()
+
+    def testDeletedDiff(self):
+        self.createFirstCommit()
+        self.modifyFile("test.txt", "")
+        self.assertGumOutputEqualsGitOutput()
+
+    def testMultiFileDiff(self):
+        self.createFirstCommit()
+        self.modifyFile("test.txt", "This is a modified test.")
+        self.createFile("newfile.txt", "This is a new file.")
+        self.assertGumOutputEqualsGitOutput()
+
+    def assertGumOutputEqualsGitOutput(self):
+        gumOutput = self.runCommand(f"{self.GUM} diff")
+        gitOutput = self.runCommand("git diff")
+        self.assertEqual(gumOutput, gitOutput)
 
 if __name__ == '__main__':
     unittest.main()
