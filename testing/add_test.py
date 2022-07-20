b509f5c0c572f0cd0be9b922175faeac5014d928
Mitra0000 <mitra11072@gmail.com>
2022-07-20 20:17:47 +0100

    Adds integration tests for the add command.

--- testing/add_test.py
+++ testing/add_test.py
@@ -1,11 +1,27 @@
-import os
 import unittest
 
 from integration import IntegrationTest
 
 # Integration tests for the `gm add` command.
 class AddTest(IntegrationTest):
-    pass
+    def testAddCommandTracksUntrackedFile(self):
+        self.createFirstCommit()
+        self.createFile("added_file.txt", "This was just added.")
+        status = self.runCommand("git status --porcelain")
+        self.assertEqual(status, "?? added_file.txt\n")
+        self.runCommand(f"{self.GUM} add")
+        status = self.runCommand("git status --porcelain")
+        self.assertEqual(status, "A  added_file.txt\n")
+    
+    def testAddSpecificFileDoesntAddAllFiles(self):
+        self.createFirstCommit()
+        self.createFile("foo.txt", "This is foo.")
+        self.createFile("bar.txt", "This is bar.")
+        status = self.runCommand("git status --porcelain")
+        self.assertEqual(status, "?? bar.txt\n?? foo.txt\n")
+        self.runCommand(f"{self.GUM} add foo.txt")
+        status = self.runCommand("git status --porcelain")
+        self.assertEqual(status, "A  foo.txt\n?? bar.txt\n")
 
 if __name__ == '__main__':
     unittest.main()
