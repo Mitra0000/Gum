64615f0571158f646af3a49b15cfd25efa805ff3
Mitra0000 <mitra11072@gmail.com>
2023-04-12 16:00:05 +0100

    First implementation of chromium-gerrit xanthan.

--- src/xanthan/chromium-gerrit.py
+++ src/xanthan/chromium-gerrit.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import importlib.util
-import os
 import subprocess
-import sys
 
 from cl import CL
 from xanthan import Xanthan
@@ -25,18 +22,29 @@ class ChromiumGerrit(Xanthan):
         self._branchesToCLs: dict[str, CL] = {}
         self._initGitCl()
     
-    def _updateState():
-        pass
+    def _updateState(self):
+        self._branchesToCLs = {}
+        output = self._runGitCommand("git cl status --no-branch-color")
+        output = output.split("\n")[1:]
+        for i in range(len(output)):
+            if output[i] == "":
+                break
+
+            data = output[i].strip()
+            if data.startswith("*"):
+                data = data[2:]
+            data = data.split()
+            self._branchesToCLs[data[0]] = CL(data[2], data[3][1:-1])
         
     # Override
     # Nullable
-    def getUrlForBranch(self, branchName: str) -> CL:
-        print(self._git_cl.get_cl_statuses([branchName], True))
-        self._runGitCommand("git cl status --no-branch-color --field=url")[:-1]
+    def getCLForBranch(self, branchName: str) -> CL:
+        self._updateState()
+        return self._branchesToCLs[branchName]
 
     # Override
     def uploadChanges(self) -> None:
-        pass
+        self._runGitCommand("git cl upload -T")
         
     def _runGitCommand(self, command: str):
         process = subprocess.Popen(command.split(),
@@ -44,14 +52,3 @@ class ChromiumGerrit(Xanthan):
                                    stderr=subprocess.PIPE)
         out, _ = process.communicate()
         return out.decode("utf-8")
-    
-    def _initGitCl(self):
-        """
-            Performs some basic reflection to access the methods inside depot 
-            tools' git_cl.py.
-        """
-        path = os.path.join(os.getcwd(), "third_party", "depot_tools", "git_cl.py")
-        spec = importlib.util.spec_from_file_location("git_cl", path)
-        self._git_cl = importlib.util.module_from_spec(spec)
-        sys.modules["git_cl"] = self._git_cl
-        spec.loader.exec_module(self._git_cl)
