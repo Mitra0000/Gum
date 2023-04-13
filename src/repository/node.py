632a82d5ac72c9c718e278f13f87e04545f83dae
Alex Mitra <alexmitra@chromium.org>
2023-04-13 18:30:36 +0100

    Converts node into a dataclass

--- src/repository/node.py
+++ src/repository/node.py
@@ -12,25 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from dataclasses import dataclass
 
+
+@dataclass
 class Node:
-
-    def __init__(self, branch, commit, parent, children, is_owned, commitPrefix,
-                 commitSuffix):
-        # The branch name eg. "aaaaa".
-        self.branch = branch
-        # The full commit hash.
-        self.commit = commit
-        # The parent commit node.
-        self.parent = parent
-        # List of child commit nodes.
-        self.children = children
-        # Boolean denoting whether the change is owned.
-        self.is_owned = is_owned
-        # Shortest unique commit hash prefix.
-        self.commitPrefix = commitPrefix
-        # Remainder of the shortened commit hash.
-        self.commitSuffix = commitSuffix
-
-    def __str__(self):
-        return f"{{branch: {self.branch}, commit: {self.commit}, parent: {self.parent.branch if self.parent else None}, children: {[c.commit for c in self.children]}, is_owned: {self.is_owned}, commitPrefix: {self.commitPrefix}, commitSuffix: {self.commitSuffix}}}"
+    # The branch name eg. "aaaaa".
+    branch: str
+    # The full commit hash.
+    commit: str
+    # The parent commit node.
+    parent: "Node"
+    # List of child commit nodes.
+    children: list["Node"]
+    # Boolean denoting whether the change is owned.
+    is_owned: bool
+    # Shortest unique commit hash prefix.
+    commitPrefix: str
+    # Remainder of the shortened commit hash.
+    commitSuffix: str
